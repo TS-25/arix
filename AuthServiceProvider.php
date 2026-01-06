aad7d308a0b2ecb4029ae418427d5b8b78407fa0
Tanvir sami <samitanvir336@gmail.com>
2026-01-06 22:52:25 +0600

    Create AuthServiceProvider.php

---
+++ AuthServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Pterodactyl\Providers;
+
+use Laravel\Sanctum\Sanctum;
+use Pterodactyl\Models\ApiKey;
+use Pterodactyl\Models\Server;
+use Pterodactyl\Policies\ServerPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    protected $policies = [
+        Server::class => ServerPolicy::class,
+    ];
+
+    public function boot(): void
+    {
+        $this->registerPolicies();
+
+        // Use Pterodactyl ApiKey model for Sanctum
+        Sanctum::usePersonalAccessTokenModel(ApiKey::class);
+    }
+}
